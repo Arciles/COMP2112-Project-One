f471468bffc45fa9e479f2cffdba7aac391b3c40
Esat IBIS <esat.taha.ibis@gmail.com>
2016-10-25 15:17:24 -0400

    gitignore added to the project

--- js/main.js
+++ js/main.js
@@ -1,7 +1,8 @@
 (function () {
   //console.log("Test");
-  var form = document.querySelector('form')
+  var form = document.querySelector('form');
   form.addEventListener('submit', function (event) {
     event.preventDefault()
-  })
-})()
+  });
+  
+})();
