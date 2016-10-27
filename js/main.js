2f21776963ee92676a64f35d3e84ad670ab8e1b8
IBIS, Esat <esatibis@expertech.net>
2016-10-27 09:48:20 -0400

    Javascript side of the project is done

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 (function () {
   //console.log("Test");
   let form = document.querySelector('form');
-  let jsonTodo ;
+  let jsonTodo;
   let isDirty = false;
 
   // first ajax call to the php to get the existing todos from todos.json
@@ -13,85 +13,142 @@
     error: todoError
   });
 
-  var readAndRefreshTodos = function (todos) {
+  function readAndRefreshTodos(todos) {
 
     jsonTodo = JSON.parse(todos);
     let todosSection = document.querySelector('.panel-body');
     todosSection.innerHTML = "";
-    // Mark the document dirty so it can be ready to be written back when its needed
-    isDirty = true;
-    // loop thru all the todos in the todos list
-    jsonTodo.todos.forEach( function (todo, index) {
-      console.log(todo);
+    // chek jsonTodo if it's emty or not
+    // display nothing to see message if it's emty
+    console.log(jsonTodo);
+    if (jsonTodo == null) {
+      let nothingToSee = `<h3>There is no TO-DO to display</h3>`;
+      todosSection.innerHTML = nothingToSee;
+      isDirty = true;
+    } else {
+      // Mark the document dirty so it can be ready to be written back when its needed
+      isDirty = true;
+      // loop thru all the todos in the todos list
+      jsonTodo.todos.forEach( function (todo, index) {
+        //console.log(todo);
 
-      // check todo is completed or not based on that add the crossed class
-      // TODO: change this if you can find a more elegant way to do it
-      let todoTemplate;
-      if (todo.isCompleted == "true") {
-        todoTemplate =
-        `<div class="media" data-id="${todo.id}">
-            <div class="media-left">
+        // check todo is completed or not based on that add the crossed class
+        // TODO: change this if you can find a more elegant way to do it
+        let todoTemplate;
+        if (todo.isCompleted == "true") {
+          todoTemplate =
+          `<div class="media" data-id="${todo.id}">
+              <div class="media-left">
+                <a href="#">
+                  <i class="fa fa-3x fa-check" aria-hidden="true"></i>
+                </a>
+              </div>
+              <div class="media-body">
+                <h4 class="media-heading crossed">${todo.title}</h4>
+                <p class="crossed">${todo.message}</p>
+              </div>
+              <div class="media-right">
               <a href="#">
-                <i class="fa fa-3x fa-check" aria-hidden="true"></i>
+                <i class="fa fa-2x fa-times" aria-hidden="true"></i>
               </a>
-            </div>
-            <div class="media-body">
-              <h4 class="media-heading crossed">${todo.title}</h4>
-              <p class="crossed">${todo.message}</p>
-            </div>
-            <div class="media-right">
-            <a href="#">
-              <i class="fa fa-2x fa-times" aria-hidden="true"></i>
-            </a>
-            </div>
-          </div>`;
+              </div>
+            </div>`;
 
-      } else {
-        todoTemplate =
-        `<div class="media" data-id="${todo.id}">
-            <div class="media-left">
+        } else {
+          todoTemplate =
+          `<div class="media" data-id="${todo.id}">
+              <div class="media-left">
+                <a href="#">
+                  <i class="fa fa-3x fa-chevron-right" aria-hidden="true"></i>
+                </a>
+              </div>
+              <div class="media-body">
+                <h4 class="media-heading">${todo.title}</h4>
+                <p>${todo.message}</p>
+              </div>
+              <div class="media-right">
               <a href="#">
-                <i class="fa fa-3x fa-chevron-right" aria-hidden="true"></i>
+                <i class="fa fa-2x fa-times" aria-hidden="true"></i>
               </a>
-            </div>
-            <div class="media-body">
-              <h4 class="media-heading">${todo.title}</h4>
-              <p>${todo.message}</p>
-            </div>
-            <div class="media-right">
-            <a href="#">
-              <i class="fa fa-2x fa-times" aria-hidden="true"></i>
-            </a>
-            </div>
-          </div>`;
-      }
-      todosSection.innerHTML += todoTemplate;
-    });
-    
-  };
-
-  readAndRefreshTodos.when(function () {
-    var completedLinks = document.querySelectorAll('.media-left a');
-
-    console.log(completedLinks);
-    completedLinks.forEach(function (todoMedia) {
-      todoMedia.addEventListener('click',markAsCompleted);
-    });
-
-    function markAsCompleted(event) {
-      let target = event.target;
-      let todoID = target.getAttribute('data-id');
-
-      // loop through the array the find the object contains the id
-      // and change to is completed based on the first state of the variable
-      jsonTodo.todos.forEach(function (todo) {
-        if(todo.id == todoID) {
-          todo.isCompleted = todo.isCompleted != "true";
+              </div>
+            </div>`;
         }
+        todosSection.innerHTML += todoTemplate;
       });
-      readAndRefreshTodos(jsonTodo);
-    }
-  });
+        markTodos();
+        deleteTodo();
+    } // end of else statement
+  }
+
+  function markTodos() {
+    var completedLinks = document.querySelectorAll('.media-left a');
+    //console.log(completedLinks);
+    completedLinks.forEach(function (todoMedia) {
+      todoMedia.addEventListener('click', function (event) {
+          let anchor = event.target;
+          //console.log(anchor.parentElement.parentElement.parentElement);
+
+          // TODO: not a good practive find more elegant way to do it
+          let todoID = anchor.parentElement.parentElement.parentElement.getAttribute('data-id');
+          //console.log(" this is supposed to be the ids" + todoID);
+          // loop through the array the find the object contains the id
+          // and change to is completed based on the first state of the variable
+          jsonTodo.todos.forEach(function (todo) {
+            if(todo.id == todoID) {
+              todo.isCompleted = todo.isCompleted != "true";
+            }
+          });
+          // make an ajax call to save the data to the json file
+          $.ajax({
+            url: "todos.php",
+            type: "POST",
+            dataType : "json",
+            data: {
+                    todos: jsonTodo
+                },
+            success: readAndRefreshTodos,
+            error: todoError
+          });
+
+      }); // end of click listener
+    }); // end of forEach
+}
+
+function deleteTodo() {
+  var deleteLinks = document.querySelectorAll('.media-right a');
+  //console.log(completedLinks);
+  deleteLinks.forEach(function (todoMedia) {
+    todoMedia.addEventListener('click', function (event) {
+        let anchor = event.target;
+        //console.log(anchor.parentElement.parentElement.parentElement);
+
+        // TODO: not a good practive find more elegant way to do it
+        let todoID = anchor.parentElement.parentElement.parentElement.getAttribute('data-id');
+        //console.log(" this is supposed to be the ids" + todoID);
+        // loop through the array the find the object contains the id
+        // and change to is completed based on the first state of the variable
+        jsonTodo.todos.forEach(function (todo, index) {
+          if(todo.id == todoID) {
+            jsonTodo.todos.splice(index, 1);
+            console.log(jsonTodo);
+          }
+        });
+        // make an ajax call to save the data to the json file
+        $.ajax({
+          url: "todos.php",
+          type: "POST",
+          dataType : "json",
+          data: {
+                  todos: jsonTodo
+              },
+          success: readAndRefreshTodos,
+          error: todoError
+        });
+
+    }); // end of click listener
+  }); // end of forEach
+}
+
 
 
   function todoError(err) {
@@ -107,12 +164,15 @@
       // create a new javasciprt object that contains all of the relevent info and push
       // it to the existing json todos array
       let tempJson = {
-          id : jsonTodo.todos.length,
+          id : (jsonTodo == null) ? 0 : jsonTodo.todos.length,
           title: document.querySelector('#txtTitle').value,
           message : document.querySelector('#txtMessage').value,
           date : document.querySelector('#txtDate').value,
           isCompleted : false
       };
+      if (jsonTodo == null) {
+        jsonTodo = {todos:[]};
+      }
       jsonTodo.todos.push(tempJson);
 
       // make a post call to todos.php for saving the new todo just added to the list
--- todos.php
+++ todos.php
@@ -1,9 +1,19 @@
 <?php
 
- if (!empty($_POST['todos'])) {
+// get the request method
+$method = $_SERVER['REQUEST_METHOD'];
+switch ($method) {
+  case "POST":
     $json = json_encode($_POST['todos']);
     file_put_contents('todos.json', $json);
- }
+    break;
+  case "DELETE":
+  // it's redundant now but in the future this part can be customized to DELETE
+  // the recors from database or something
+    $json = json_encode($_POST['todos']);
+    file_put_contents('todos.json', $json);
+  break;
+}
    // copy file content into a string var.
    $json_file = file_get_contents('todos.json');
    // convert the string to a json object.
